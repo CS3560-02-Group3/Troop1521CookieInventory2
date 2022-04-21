c7f9a8559b26cf85d9aed6c2564639ca72f935b1
fdhernandez <rodriguezhernandez0406@gmail.com>
2022-04-21 14:08:08 -0700

    adding cookie order to menu

--- mainMenu.Designer.vb
+++ mainMenu.Designer.vb
@@ -28,10 +28,14 @@ Partial Class mainMenu
         Me.TabPage2 = New System.Windows.Forms.TabPage()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
         Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.TabPage3 = New System.Windows.Forms.TabPage()
+        Me.DataGridView2 = New System.Windows.Forms.DataGridView()
         Me.TabControl1.SuspendLayout()
         Me.TabPage1.SuspendLayout()
         Me.TabPage2.SuspendLayout()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TabPage3.SuspendLayout()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'TabControl1
@@ -42,11 +46,12 @@ Partial Class mainMenu
         Me.TabControl1.Appearance = System.Windows.Forms.TabAppearance.Buttons
         Me.TabControl1.Controls.Add(Me.TabPage1)
         Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Controls.Add(Me.TabPage3)
         Me.TabControl1.Font = New System.Drawing.Font("Segoe UI Semibold", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.TabControl1.Location = New System.Drawing.Point(0, -1)
         Me.TabControl1.Name = "TabControl1"
         Me.TabControl1.SelectedIndex = 0
-        Me.TabControl1.Size = New System.Drawing.Size(1830, 736)
+        Me.TabControl1.Size = New System.Drawing.Size(1541, 736)
         Me.TabControl1.TabIndex = 0
         '
         'TabPage1
@@ -55,7 +60,7 @@ Partial Class mainMenu
         Me.TabPage1.Location = New System.Drawing.Point(4, 33)
         Me.TabPage1.Name = "TabPage1"
         Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage1.Size = New System.Drawing.Size(793, 414)
+        Me.TabPage1.Size = New System.Drawing.Size(1533, 699)
         Me.TabPage1.TabIndex = 0
         Me.TabPage1.Text = "Home"
         Me.TabPage1.UseVisualStyleBackColor = True
@@ -75,7 +80,7 @@ Partial Class mainMenu
         Me.TabPage2.Location = New System.Drawing.Point(4, 33)
         Me.TabPage2.Name = "TabPage2"
         Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage2.Size = New System.Drawing.Size(1822, 699)
+        Me.TabPage2.Size = New System.Drawing.Size(1533, 699)
         Me.TabPage2.TabIndex = 1
         Me.TabPage2.Text = "Girls"
         Me.TabPage2.UseVisualStyleBackColor = True
@@ -88,11 +93,30 @@ Partial Class mainMenu
         Me.DataGridView1.Size = New System.Drawing.Size(1808, 664)
         Me.DataGridView1.TabIndex = 2
         '
+        'TabPage3
+        '
+        Me.TabPage3.Controls.Add(Me.DataGridView2)
+        Me.TabPage3.Location = New System.Drawing.Point(4, 33)
+        Me.TabPage3.Name = "TabPage3"
+        Me.TabPage3.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage3.Size = New System.Drawing.Size(1533, 699)
+        Me.TabPage3.TabIndex = 2
+        Me.TabPage3.Text = "Cookie Order"
+        Me.TabPage3.UseVisualStyleBackColor = True
+        '
+        'DataGridView2
+        '
+        Me.DataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView2.Location = New System.Drawing.Point(3, 0)
+        Me.DataGridView2.Name = "DataGridView2"
+        Me.DataGridView2.Size = New System.Drawing.Size(1527, 696)
+        Me.DataGridView2.TabIndex = 0
+        '
         'mainMenu
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1829, 735)
+        Me.ClientSize = New System.Drawing.Size(1540, 735)
         Me.Controls.Add(Me.TabControl1)
         Me.Name = "mainMenu"
         Me.Text = "Troop 1521 Cookie Inventory"
@@ -101,6 +125,8 @@ Partial Class mainMenu
         Me.TabPage1.PerformLayout()
         Me.TabPage2.ResumeLayout(False)
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.TabPage3.ResumeLayout(False)
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -111,4 +137,6 @@ Partial Class mainMenu
     Friend WithEvents TabPage2 As TabPage
     Friend WithEvents DataGridView1 As DataGridView
     Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents TabPage3 As TabPage
+    Friend WithEvents DataGridView2 As DataGridView
 End Class
--- mainMenu.vb
+++ mainMenu.vb
@@ -30,4 +30,24 @@ Public Class mainMenu
         myForm.Show()
 
     End Sub
+
+    Private Sub DataGridView2_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView2.CellContentClick
+        If e.RowIndex = -1 Then
+            Return
+        End If
+        mainMenu_Load(e, e)
+        Dim selectedRow As DataGridViewRow
+        selectedRow = DataGridView1.Rows(e.RowIndex)
+        Dim myForm As New cookieOrder
+        myForm.orderIDTE.Text = selectedRow.Cells(0).Value
+        myForm.userIDTE.Text = selectedRow.Cells(0).Value
+        myForm.inventoryIDTE.Text = selectedRow.Cells(1).Value
+        myForm.yearCookieIDTE.Text = selectedRow.Cells(2).Value
+        myForm.dateInfo.Text = selectedRow.Cells(3).Value
+        myForm.orderQTB.Text = selectedRow.Cells(4).Value
+        myForm.pickupQTB.Text = selectedRow.Cells(5).Value
+        myForm.returnQTB.Text = selectedRow.Cells(6).Value
+        myForm.cookieNTB.Text = selectedRow.Cells(7).Value
+        myForm.Show()
+    End Sub
 End Class
