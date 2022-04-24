6f9e3e9ac230514e18a288ae908eef6a11496062
britanyflores <britanyflores71@gmail.com>
2022-04-24 00:36:59 -0700

    Inventory class completed

--- inventoryForm.Designer.vb
+++ inventoryForm.Designer.vb
@@ -22,27 +22,18 @@ Partial Class inventoryForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.Label4 = New System.Windows.Forms.Label()
-        Me.Label5 = New System.Windows.Forms.Label()
-        Me.Label6 = New System.Windows.Forms.Label()
+        Me.inventoryID = New System.Windows.Forms.Label()
+        Me.warehouseID = New System.Windows.Forms.Label()
+        Me.yearCookieID = New System.Windows.Forms.Label()
         Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
         Me.Label7 = New System.Windows.Forms.Label()
         Me.Label8 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.inQuantityTB = New System.Windows.Forms.TextBox()
         Me.Label9 = New System.Windows.Forms.Label()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.submit = New System.Windows.Forms.Button()
+        Me.noteTE = New System.Windows.Forms.RichTextBox()
         Me.SuspendLayout()
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(421, 104)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(0, 20)
-        Me.Label1.TabIndex = 0
         '
         'Label2
         '
@@ -54,49 +45,41 @@ Partial Class inventoryForm
         Me.Label2.TabIndex = 1
         Me.Label2.Text = "Inventory"
         '
-        'Label3
+        'inventoryID
         '
-        Me.Label3.AutoSize = True
-        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 12.0!)
-        Me.Label3.Location = New System.Drawing.Point(68, 143)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(146, 32)
-        Me.Label3.TabIndex = 2
-        Me.Label3.Text = "Inventory ID"
+        Me.inventoryID.AutoSize = True
+        Me.inventoryID.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.inventoryID.Location = New System.Drawing.Point(68, 143)
+        Me.inventoryID.Name = "inventoryID"
+        Me.inventoryID.Size = New System.Drawing.Size(146, 32)
+        Me.inventoryID.TabIndex = 2
+        Me.inventoryID.Text = "Inventory ID"
         '
-        'Label4
+        'warehouseID
         '
-        Me.Label4.AutoSize = True
-        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 12.0!)
-        Me.Label4.Location = New System.Drawing.Point(437, 143)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(164, 32)
-        Me.Label4.TabIndex = 3
-        Me.Label4.Text = "Warehouse ID"
+        Me.warehouseID.AutoSize = True
+        Me.warehouseID.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.warehouseID.Location = New System.Drawing.Point(437, 143)
+        Me.warehouseID.Name = "warehouseID"
+        Me.warehouseID.Size = New System.Drawing.Size(164, 32)
+        Me.warehouseID.TabIndex = 3
+        Me.warehouseID.Text = "Warehouse ID"
         '
-        'Label5
+        'yearCookieID
         '
-        Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(738, 155)
-        Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(0, 20)
-        Me.Label5.TabIndex = 4
-        '
-        'Label6
-        '
-        Me.Label6.AutoSize = True
-        Me.Label6.Font = New System.Drawing.Font("Segoe UI", 12.0!)
-        Me.Label6.Location = New System.Drawing.Point(819, 143)
-        Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(170, 32)
-        Me.Label6.TabIndex = 5
-        Me.Label6.Text = "Year Cookie ID"
+        Me.yearCookieID.AutoSize = True
+        Me.yearCookieID.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.yearCookieID.Location = New System.Drawing.Point(819, 143)
+        Me.yearCookieID.Name = "yearCookieID"
+        Me.yearCookieID.Size = New System.Drawing.Size(170, 32)
+        Me.yearCookieID.TabIndex = 5
+        Me.yearCookieID.Text = "Year Cookie ID"
         '
         'DateTimePicker1
         '
         Me.DateTimePicker1.CalendarFont = New System.Drawing.Font("Segoe UI", 12.0!)
         Me.DateTimePicker1.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.DateTimePicker1.Location = New System.Drawing.Point(695, 340)
+        Me.DateTimePicker1.Location = New System.Drawing.Point(696, 291)
         Me.DateTimePicker1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.DateTimePicker1.Name = "DateTimePicker1"
         Me.DateTimePicker1.Size = New System.Drawing.Size(389, 39)
@@ -106,7 +89,7 @@ Partial Class inventoryForm
         '
         Me.Label7.AutoSize = True
         Me.Label7.Font = New System.Drawing.Font("Segoe UI", 12.0!)
-        Me.Label7.Location = New System.Drawing.Point(550, 337)
+        Me.Label7.Location = New System.Drawing.Point(536, 291)
         Me.Label7.Name = "Label7"
         Me.Label7.Size = New System.Drawing.Size(65, 32)
         Me.Label7.TabIndex = 11
@@ -116,72 +99,82 @@ Partial Class inventoryForm
         '
         Me.Label8.AutoSize = True
         Me.Label8.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label8.Location = New System.Drawing.Point(68, 337)
+        Me.Label8.Location = New System.Drawing.Point(68, 291)
         Me.Label8.Name = "Label8"
         Me.Label8.Size = New System.Drawing.Size(107, 32)
         Me.Label8.TabIndex = 12
         Me.Label8.Text = "Quantity"
         '
-        'TextBox1
+        'inQuantityTB
         '
-        Me.TextBox1.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox1.Location = New System.Drawing.Point(246, 340)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(117, 39)
-        Me.TextBox1.TabIndex = 13
+        Me.inQuantityTB.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.inQuantityTB.Location = New System.Drawing.Point(246, 291)
+        Me.inQuantityTB.Name = "inQuantityTB"
+        Me.inQuantityTB.Size = New System.Drawing.Size(117, 39)
+        Me.inQuantityTB.TabIndex = 13
         '
         'Label9
         '
         Me.Label9.AutoSize = True
         Me.Label9.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label9.Location = New System.Drawing.Point(68, 544)
+        Me.Label9.Location = New System.Drawing.Point(68, 430)
         Me.Label9.Name = "Label9"
         Me.Label9.Size = New System.Drawing.Size(68, 32)
         Me.Label9.TabIndex = 14
         Me.Label9.Text = "Note"
         '
-        'TextBox2
+        'submit
         '
-        Me.TextBox2.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox2.Location = New System.Drawing.Point(246, 544)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(402, 39)
-        Me.TextBox2.TabIndex = 15
+        Me.submit.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.submit.Location = New System.Drawing.Point(919, 598)
+        Me.submit.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.submit.Name = "submit"
+        Me.submit.Size = New System.Drawing.Size(144, 51)
+        Me.submit.TabIndex = 17
+        Me.submit.Text = "Submit"
+        Me.submit.UseVisualStyleBackColor = True
+        '
+        'noteTE
+        '
+        Me.noteTE.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.noteTE.Location = New System.Drawing.Point(178, 430)
+        Me.noteTE.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.noteTE.Name = "noteTE"
+        Me.noteTE.Size = New System.Drawing.Size(568, 146)
+        Me.noteTE.TabIndex = 18
+        Me.noteTE.Text = ""
         '
         'inventoryForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1144, 693)
-        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.noteTE)
+        Me.Controls.Add(Me.submit)
         Me.Controls.Add(Me.Label9)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.inQuantityTB)
         Me.Controls.Add(Me.Label8)
         Me.Controls.Add(Me.Label7)
         Me.Controls.Add(Me.DateTimePicker1)
-        Me.Controls.Add(Me.Label6)
-        Me.Controls.Add(Me.Label5)
-        Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.yearCookieID)
+        Me.Controls.Add(Me.warehouseID)
+        Me.Controls.Add(Me.inventoryID)
         Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Label1)
         Me.Name = "inventoryForm"
-        Me.Text = "Form1"
+        Me.Text = "Inventory Form"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
-    Friend WithEvents Label3 As Label
-    Friend WithEvents Label4 As Label
-    Friend WithEvents Label5 As Label
-    Friend WithEvents Label6 As Label
+    Friend WithEvents inventoryID As Label
+    Friend WithEvents warehouseID As Label
+    Friend WithEvents yearCookieID As Label
     Friend WithEvents DateTimePicker1 As DateTimePicker
     Friend WithEvents Label7 As Label
     Friend WithEvents Label8 As Label
-    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents inQuantityTB As TextBox
     Friend WithEvents Label9 As Label
-    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents submit As Button
+    Friend WithEvents noteTE As RichTextBox
 End Class
--- inventoryForm.vb
+++ inventoryForm.vb
@@ -1,47 +1,33 @@
-﻿'Imports MySql.Data.MySqlClient
-'Imports System
+﻿Imports MySql.Data.MySqlClient
 
 
 Public Class inventoryForm
+    Private Sub Submit_Click(sender As Object, e As EventArgs) Handles submit.Click
+        Dim confirmMsg = MessageBox.Show("Are you sure you want to submit?", "Submit", MessageBoxButtons.YesNo)
+        If confirmMsg = DialogResult.Yes Then
 
-    '    Private Sub Submit_Click(sender As Object, e As EventArgs) Handles submit.Click
-    '            Dim inventyID As Integer = inventoryIDTB.Text
-    '            Dim warehouseID As Integer = warehouseIDTB.Text
-    '            Dim yearCookieID As Integer = yearCookieIDTB.Text
-    '        Dim dateID As Integer = dateIDTB.Text
-    '        Dim inQuantity As Integer = inQuantityTB.Text
-    '        Dim note As String = noteTB.Text
+            Dim dateID As Integer = DateTimePicker1.Text
+            Dim inQuantity As Integer = inQuantityTB.Text
+            Dim note As String = noteTE.Text
 
-    '            Dim conn As New myConnection()
-    '            Dim command As New MySqlCommand("INSERT INTO `user`(`inventoryID`, `warehouseID`, `yearCookieID`, `data`, `inQuantity`, `note`) VALUES (@inventoryID, @warehouseID, @yearCookieID, @date, @inQuantity, @note)", conn.getConnection())
+            Dim conn As New myConnection()
+            Dim command As New MySqlCommand("INSERT INTO `user`(`dateID`, `inQuantity`, `note`) VALUES (@dateID, @inQuantity, @note)", conn.getConnection())
 
-    '            command.Parameters.Add("@inventoryID", MySqlDbType.VarChar).Value = inventoryID
-    '            command.Parameters.Add("@warehouseID", MySqlDbType.VarChar).Value = warehouseID
-    '            command.Parameters.Add("@yearCookieID", MySqlDbType.VarChar).Value = yearCookieID
-    '        command.Parameters.Add("@dateID", MySqlDbType.VarChar).Value = dateID
-    '        command.Parameters.Add("@inQuantity", MySqlDbType.VarChar).Value = inQuantity
-    '        command.Parameters.Add("@note", MySqlDbType.VarChar).Value = note
+            command.Parameters.Add("@dateID", MySqlDbType.VarChar).Value = dateID
+            command.Parameters.Add("@inQuantity", MySqlDbType.Int16).Value = inQuantity
+            command.Parameters.Add("@note", MySqlDbType.VarChar).Value = note
 
-    '            conn.openConnection()
+            conn.openConnection()
 
-    '            If command.ExecuteNonQuery() = 1 Then
-    '                MsgBox("COOKIE INSERTED INTO WAREHOUSE'S INVENTORY")
-    '                conn.closeConnection()
-    '            Else
-    '                MsgBox("COOKIE NOT INSERTED INTO WAREHOUSE'S INVENTORY")
-    '                conn.closeConnection()
-    '            End If
-    '        End Sub
+            If command.ExecuteNonQuery() = 1 Then
+                MsgBox("COOKIE INSERTED INTO WAREHOUSE'S INVENTORY")
+                conn.closeConnection()
+            Else
+                MsgBox("COOKIE NOT INSERTED INTO WAREHOUSE'S INVENTORY")
+                conn.closeConnection()
+            End If
+        End If
 
-    '    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+    End Sub
 
-    '    End Sub
-
-    '    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
-
-    '    End Sub
-
-    '    Private Sub inventoryForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    '    End Sub
 End Class
